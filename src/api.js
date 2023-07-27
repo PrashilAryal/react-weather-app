8c5612f9f32f1cbf64849701de256595d5f4a4c5
PrashilAryal <prashilaryal13@gmail.com>
2023-07-27 21:38:37 +0545

    weather check with 7 day forecast

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "content-type": "application/octet-stream",
+    "X-RapidAPI-Key": "b5f4e5fc8dmsh9f5e57b35b942d8p1648cajsn146e9c5187fa",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "e982435d4bdd70820a99ba33e7ff0cc0";
